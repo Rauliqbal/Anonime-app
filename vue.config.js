f9c48830fd63d456af10300a8ad5dbca31454369
Rauliqbal <muhamadrauliqbal.13@gmail.com>
2022-05-18 17:26:58 +0700

    [FIX]Deploy

--- vue.config.js
+++ vue.config.js
@@ -2,17 +2,4 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
    transpileDependencies: true,
    lintOnSave: false,
-   chainWebpack: (config) => {
-      config.plugin("pwa").tap((args) => {
-         return [pwaArgs];
-      });
-   },
 });
-
-var manifestJSON = require("./public/manifest.json");
-
-pwaArgs = {
-   themeColor: manifestJSON.theme_color,
-   name: manifestJSON.short_name,
-   msTileColor: manifestJSON.background_color,
-};
