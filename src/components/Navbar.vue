e72b1a8e85c888d1e5a6a56e80e291b4ab57150d
Rauliqbal <muhamadrauliqbal.13@gmail.com>
2022-05-19 12:52:38 +0700

    [FETCH] Search anime list

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -32,8 +32,17 @@
                </div>
             </div>
 
-            <div class="hidden lg:block ml-auto">
-               <input class="py-2 pl-6 rounded-full bg-gray-700 text-white outline-none border-none" type="text" placeholder="Search anime or movie" />
+            <div class="hidden ml-auto lg:flex lg:flex-col">
+               <form @submit.prevent="HandleSearch">
+                  <input class="py-2 px-6 rounded-full bg-gray-700 text-white outline-none border-none" type="search" placeholder="Search anime or movie" v-model="search_query" required />
+               </form>
+
+               <div class="absolute bg-gray-900 border border-gray-800 z-50 mt-12 rounded-xl w-[366px] overflow-hidden">
+                  <div class="flex flex-col gap-4 py-5 px-6 overflow-y-auto max-h-[36rem]" v-if="animelist.length > 0">
+                     <!-- card movie search -->
+                     <CardMovie v-for="anime in animelist" :key="anime.mal_id" :anime="anime" />
+                  </div>
+               </div>
             </div>
          </div>
       </div>
@@ -41,7 +50,31 @@
 </template>
 
 <script>
-export default {};
+import { ref } from "vue";
+import CardMovie from "./Navbar/CardMovie.vue";
+
+export default {
+   setup() {
+      const search_query = ref("");
+      const animelist = ref([]);
+
+      const HandleSearch = async () => {
+         animelist.value = await fetch(`https://api.jikan.moe/v3/search/anime?q=${search_query.value}`)
+            .then((res) => res.json())
+            .then((data) => data.results);
+
+         // console.log(animelist.value);
+         search_query.value = "";
+      };
+
+      return {
+         search_query,
+         animelist,
+         HandleSearch,
+      };
+   },
+   components: { CardMovie },
+};
 </script>
 
 <style></style>
---
+++ src/components/Navbar/CardMovie.vue
@@ -0,0 +1,14 @@
+<template>
+   <a :href="anime.url" class="flex gap-6">
+      <img :src="anime.image_url" class="w-24 h-32 object-cover rounded-xl" :alt="anime.title + 'Cover'" />
+      <h1 class="text-lg text-white">{{ anime.title }}</h1>
+   </a>
+</template>
+
+<script>
+export default {
+   props: ["anime"],
+};
+</script>
+
+<style></style>
