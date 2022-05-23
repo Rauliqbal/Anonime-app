fb11602831fdbb84794daedf5b5150223a4b74bb
Rauliqbal <muhamadrauliqbal.13@gmail.com>
2022-05-23 17:41:41 +0700

    [ADD] Anime Details

--- src/components/carousel.vue
+++ src/components/carousel.vue
@@ -11,8 +11,8 @@
       :modules="modules"
    >
       <swiper-slide>
-         <a href="" class="h-20 md:h-[25vw] cursor-pointer">
-            <img class="rounded-xl object-cover w-full h-full" src="../assets/images/anime_carousel.webp" alt="" />
+         <a href="" class="h-20 md:h-[25vw] cursor-pointer group">
+            <img class="rounded-xl object-cover w-full h-full group-hover:scale-105 transition-all duration-400 ease-out" src="../assets/images/anime_carousel.webp" alt="" />
             <span class="bg-gradient-to-r from-black/90 rounded-xl w-full h-full absolute bottom-0"></span>
             <div class="absolute bottom-0 p-4 md:p-8 w-full lg:w-1/2 z-10">
                <h1 class="text-lg md:text-3xl lg:text-4xl font-bold text-gray-200 tracking-wide">Weathering With You</h1>
@@ -23,8 +23,8 @@
          </a>
       </swiper-slide>
       <swiper-slide>
-         <a href="" class="h-20 md:h-[25vw] cursor-pointer">
-            <img class="rounded-xl object-cover w-full h-full" src="../assets/images/op_carousel.webp" alt="" />
+         <a href="/anime/one-piece" class="h-20 md:h-[25vw] cursor-pointer group">
+            <img class="rounded-xl object-cover w-full h-full group-hover:scale-105 transition-all duration-400 ease-out" src="../assets/images/op_carousel.webp" alt="" />
             <span class="bg-gradient-to-r from-black/90 rounded-xl w-full h-full absolute bottom-0"></span>
             <div class="absolute bottom-0 p-4 md:p-8 w-full lg:w-1/2 z-10">
                <h1 class="text-lg md:text-3xl lg:text-4xl font-bold text-gray-200 tracking-wide">One Piece</h1>
@@ -36,8 +36,8 @@
       </swiper-slide>
 
       <swiper-slide>
-         <a href="" class="h-20 md:h-[25vw] cursor-pointer">
-            <img class="rounded-xl object-cover w-full h-full" src="../assets/images/bnh_carousel.webp" alt="" />
+         <a href="" class="h-20 md:h-[25vw] cursor-pointer group">
+            <img class="rounded-xl object-cover w-full h-full group-hover:scale-105 transition-all duration-400 ease-out" src="../assets/images/bnh_carousel.webp" alt="" />
             <span class="bg-gradient-to-r from-black/90 rounded-xl w-full h-full absolute bottom-0"></span>
             <div class="absolute bottom-0 p-4 md:p-8 w-full lg:w-1/2 z-10">
                <h1 class="text-lg md:text-3xl lg:text-4xl font-bold text-gray-200 tracking-wide">My Hero Academia</h1>
--- src/views/animeview/Onepiece.vue
+++ src/views/animeview/Onepiece.vue
@@ -1,51 +1,51 @@
 <template>
    <div class="container">
       <Navbar />
-      <div class="grid grid-cols-6 gap-8 px-6 lg:px-8 my-8">
-         <div class="col-span-full lg:col-span-4">
-            <h1 class="text-2xl md:text-3xl lg:text-4xl text-white capitalize font-semibold">One Piece - Episode 1018</h1>
+      <div class="grid grid-cols-7 gap-8 px-6 lg:px-8 my-8">
+         <div class="col-span-full lg:col-span-5">
+            <h1 class="text-xl md:text-3xl lg:text-4xl text-white capitalize font-semibold">One Piece - Episode 1018</h1>
             <div class="rounded-xl overflow-hidden aspect-video mt-5">
                <iframe
                   class="w-full h-full"
-                  src="https://www.youtube.com/embed/JVD_1uJ6vwo"
-                  title="YouTube video player"
+                  src="https://www.youtube.com/embed/xLL3azdaKJg"
+                  title="One piece episode 1018"
                   frameborder="0"
                   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                   allowfullscreen
                ></iframe>
             </div>
 
-            <div class="grid grid-cols-6 gap-8 mt-8">
-               <div class="col-span-2">
-                  <img class="w-full h-[354px] rounded-xl overflow-hidden object-cover" src="@/assets/images/op_card.jpg" alt="" />
+            <div class="grid grid-cols-6 gap-4 md:gap-8 mt-8">
+               <div class="col-span-3 lg:col-span-2">
+                  <img class="w-full h-[260px] lg:h-[354px] rounded-xl overflow-hidden object-cover" src="@/assets/images/op_card.jpg" alt="" />
                </div>
 
-               <div class="col-span-3">
+               <div class="col-span-3 lg:col-span-3">
                   <div class="grid gap-4">
                      <div class="flex flex-col">
-                        <h2 class="text-xl text-white font-semibold">Type :</h2>
-                        <p class="text-lg text-gray-400">TV</p>
+                        <h2 class="text-base lg:text-xl text-white font-semibold">Type :</h2>
+                        <p class="text-xs lg:text-lg text-gray-400">TV</p>
                      </div>
                      <div class="flex flex-col">
-                        <h2 class="text-xl text-white font-semibold">Status :</h2>
-                        <p class="text-lg text-gray-400 capitalize">ongoing</p>
+                        <h2 class="text-base lg:text-xl text-white font-semibold">Status :</h2>
+                        <p class="text-xs lg:text-lg text-gray-400 capitalize">ongoing</p>
                      </div>
                      <div class="flex flex-col">
-                        <h2 class="text-xl text-white font-semibold">Studios :</h2>
-                        <p class="text-lg text-gray-400 capitalize">toei animation</p>
+                        <h2 class="text-base lg:text-xl text-white font-semibold">Studios :</h2>
+                        <p class="text-xs lg:text-lg text-gray-400 capitalize">toei animation</p>
                      </div>
                      <div class="flex flex-col">
-                        <h2 class="text-xl text-white font-semibold">Duration :</h2>
-                        <p class="text-lg text-gray-400 capitalize">24 min.</p>
+                        <h2 class="text-base lg:text-xl text-white font-semibold">Duration :</h2>
+                        <p class="text-xs lg:text-lg text-gray-400 capitalize">24 min.</p>
                      </div>
                      <div class="flex flex-col">
-                        <h2 class="text-xl text-white font-semibold">Genres :</h2>
-                        <p class="text-lg text-gray-400 capitalize">Action, Adventure, Fantasy</p>
+                        <h2 class="text-base lg:text-xl text-white font-semibold">Genres :</h2>
+                        <p class="text-xs lg:text-lg text-gray-400 capitalize">Action, Adventure, Fantasy</p>
                      </div>
                   </div>
                </div>
 
-               <div class="col-span-1">
+               <div class="hidden md:block col-span-1">
                   <div class="py-1 px-4 text-lg bg-gray-900 border border-gray-600 text-white text-center rounded-xl">
                      <p class="text-center text-base tracking-wide -mb-1">score</p>
                      <span class="">8.64</span>
@@ -54,8 +54,8 @@
             </div>
 
             <div class="flex flex-col mt-8">
-               <h2 class="text-xl text-white font-semibold">Sypnopsis :</h2>
-               <p class="text-lg text-gray-400 capitalize mt-1">
+               <h2 class="text-base lg:text-xl text-white font-semibold">Sypnopsis :</h2>
+               <p class="text-sm lg:text-lg text-gray-400 capitalize mt-1 leading-relaxed">
                   Gol D. Roger was known as the "Pirate King," the strongest and most infamous being to have sailed the Grand Line. The capture and execution of Roger by the World Government brought a change throughout the world. His last
                   words before his death revealed the existence of the greatest treasure in the world, One Piece. It was this revelation that brought about the Grand Age of Pirates, men who dreamed of finding One Piece—which promises an
                   unlimited amount of riches and fame—and quite possibly the pinnacle of glory and the title of the Pirate King.
@@ -63,13 +63,65 @@
             </div>
          </div>
 
-         <div class="col-span-full lg:col-span-2">Populer Anime</div>
+         <div class="col-span-full lg:col-span-2">
+            <h1 class="text-xl md:text-3xl lg:text-4xl text-white capitalize font-semibold">Popular Anime</h1>
+            <div class="grid grid-cols-4 gap-y-6 gap-x-4 mt-5">
+               <router-link to="/anime/captain-tsubasa" class="col-span-2 flex flex-col rounded-xl items-center overflow-hidden group relative">
+                  <img class="object-cover w-full max-h-[254px] h-full" src="@/assets/images/captsu_card.webp" alt="" />
+                  <span class="bg-gradient-to-t from-black w-full h-full flex flex-col justify-end items-center absolute py-2 px-1 text-gray-300 opacity-0 group-hover:opacity-100 transition-all duration-300 ease-in-out">
+                     <h1 class="text-sm text-white text-center">Captain Tsubasa</h1>
+                     <p class="text-xs text-gray-400">Episode 20</p>
+                  </span>
+               </router-link>
+
+               <router-link to="/anime/captain-tsubasa" class="col-span-2 flex flex-col rounded-xl items-center overflow-hidden group relative">
+                  <img class="object-cover w-full max-h-[254px] h-full" src="@/assets/images/borto_card.png" alt="" />
+                  <span class="bg-gradient-to-t from-black w-full h-full flex flex-col justify-end items-center absolute py-2 px-1 text-gray-300 opacity-0 group-hover:opacity-100 transition-all duration-300 ease-in-out">
+                     <h1 class="text-sm text-white text-center">Boruto Naruto..</h1>
+                     <p class="text-xs text-gray-400">Episode 250</p>
+                  </span>
+               </router-link>
+
+               <router-link to="/anime/captain-tsubasa" class="col-span-2 flex flex-col rounded-xl items-center overflow-hidden group relative">
+                  <img class="object-cover w-full max-h-[254px] h-full" src="@/assets/images/spy_card.png" alt="" />
+                  <span class="bg-gradient-to-t from-black w-full h-full flex flex-col justify-end items-center absolute py-2 px-1 text-gray-300 opacity-0 group-hover:opacity-100 transition-all duration-300 ease-in-out">
+                     <h1 class="text-sm text-white text-center">Spy X Family</h1>
+                     <p class="text-xs text-gray-400">Episode 07</p>
+                  </span>
+               </router-link>
+
+               <router-link to="/anime/captain-tsubasa" class="col-span-2 flex flex-col rounded-xl items-center overflow-hidden group relative">
+                  <img class="object-cover w-full max-h-[254px] h-full" src="@/assets/images/tomodachi_card.webp" alt="" />
+                  <span class="bg-gradient-to-t from-black w-full h-full flex flex-col justify-end items-center absolute py-2 px-1 text-gray-300 opacity-0 group-hover:opacity-100 transition-all duration-300 ease-in-out">
+                     <h1 class="text-sm text-white text-center">Tomodachi..</h1>
+                     <p class="text-xs text-gray-400">Episode 07</p>
+                  </span>
+               </router-link>
+
+               <router-link to="/anime/captain-tsubasa" class="col-span-2 flex flex-col rounded-xl items-center overflow-hidden group relative">
+                  <img class="object-cover w-full max-h-[254px] h-full" src="@/assets/images/myhero_card.webp" alt="" />
+                  <span class="bg-gradient-to-t from-black w-full h-full flex flex-col justify-end items-center absolute py-2 px-1 text-gray-300 opacity-0 group-hover:opacity-100 transition-all duration-300 ease-in-out">
+                     <h1 class="text-sm text-white text-center">My Hero Academia</h1>
+                     <p class="text-xs text-gray-400">Episode 25</p>
+                  </span>
+               </router-link>
+
+               <router-link to="/anime/captain-tsubasa" class="col-span-2 flex flex-col rounded-xl items-center overflow-hidden group relative">
+                  <img class="object-cover w-full max-h-[254px] h-full" src="@/assets/images/tokrev_card.webp" alt="" />
+                  <span class="bg-gradient-to-t from-black w-full h-full flex flex-col justify-end items-center absolute py-2 px-1 text-gray-300 opacity-0 group-hover:opacity-100 transition-all duration-300 ease-in-out">
+                     <h1 class="text-sm text-white text-center">Tokyo Revengers</h1>
+                     <p class="text-xs text-gray-400">Episode 24</p>
+                  </span>
+               </router-link>
+            </div>
+         </div>
       </div>
    </div>
 </template>
 
 <script>
 import Navbar from "@/components/Navbar.vue";
+
 export default { components: { Navbar } };
 </script>
 
